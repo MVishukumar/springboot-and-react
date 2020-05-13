16f7a1ce8a171b352cd27df3cfd440cec72eb427
Vishukumar M <vishukumar.m@ericsson.com>
2020-05-13 15:05:25 +0530

    more commit

---
+++ target/classes/public/precache-manifest.6ce5ea40611e5de99e6cb86da03c0aa1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e36699568ad497f9ba9c5b5e8c09cd6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18e83cba8585a00a976d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "be5789c5d075f03b6b17",
+    "url": "/static/js/2.89e35ff1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.89e35ff1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18e83cba8585a00a976d",
+    "url": "/static/js/main.be75dc5b.chunk.js"
+  },
+  {
+    "revision": "018907eede1d1da6931e",
+    "url": "/static/js/runtime-main.98b4df4c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
