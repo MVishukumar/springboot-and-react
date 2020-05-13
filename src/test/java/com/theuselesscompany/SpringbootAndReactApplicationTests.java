803030d07db7755ce65c5a771498889b5db87042
Vishukumar M <vishukumar.m@ericsson.com>
2020-05-13 14:53:10 +0530

    Initial commit

---
+++ src/test/java/com/theuselesscompany/SpringbootAndReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theuselesscompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAndReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
